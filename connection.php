fa8dce56a7ddb40865713e02be1b8d0312aaee7a
remsmalopit69 <remielandrei.mortiz.cics@ust.edu.ph>
2025-05-20 14:20:40 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,17 +2,17 @@
 
 $servername = "localhost";
 $username = "root";
-$password = "";
-$database = "dti_schema";
+$password = "1234";
+$database = "user";
 
 
 $conn = new mysqli($servername, $username, $password, $database);
 
-/*if ($conn->connect_error) {
-    echo "Connection failed";
-} else{
-    echo "Connection success";
-}*/
+// if ($conn->connect_error) {
+//     echo "Connection failed";
+// } else{
+//     echo "Connection success";
+// }
 //tinggal ko muna ung conn-error kasi yun ang error dito sa mac
 
 ?>
