8f6e5a39d641fec09bf55d1e9f1148a1a01f83a8
Cake <90282274+ichigokekiii@users.noreply.github.com>
2025-05-31 22:00:17 +0800

    Fixed conflicts

---
+++ connection.php
@@ -0,0 +1,18 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "user";
+
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+// if ($conn->connect_error) {
+//     echo "Connection failed";
+// } else{
+//     echo "Connection success";
+// }
+//tinggal ko muna ung conn-error kasi yun ang error dito sa mac
+
+?>
