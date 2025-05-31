15a8bf96ad62806a8b98d9027f145b5c3da0356f
Cake <90282274+ichigokekiii@users.noreply.github.com>
2025-05-31 21:59:22 +0800

    login update

---
+++ login.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Admin Login - Dress to Impress</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="login.css">
+</head>
+
+<body>
+    <div class="login-container">
+        <div class="login-form">
+            <h1>Welcome, Admin!</h1>
+            <p>Please log in</p>
+            
+            <form action="login.php" method="post">
+                <div class="form-group">
+                    <input type="text" name="username" id="uname" placeholder="Username">
+                </div>
+                
+                <div class="form-group">
+                    <input type="password" name="password" id="pw" placeholder="Password">
+                </div>
+                
+                <button type="submit" name="submit" class="login-btn">Login</button>
+            </form>
+        </div>
+        
+        <div class="image-section-admin"></div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
+
+<?php
+include "connection.php";
+
+if (isset($_POST['submit'])) {
+    session_start();
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $_SESSION['username'] = $username;
+
+    $sql = "SELECT * FROM users_table WHERE username='" . $username . "' AND password='" . $password . "'";
+
+    $result = $conn->query($sql);
+
+    if ($result->num_rows == 1) {
+        $field = $result->fetch_assoc();
+        $role = $field['userType'];
+
+        if ($role == "Judge") {
+            header("location: userHome.php");
+        } else if ($role == "Admin") {
+            header("location: admin_dashboard.php");
+        } else if ($role == "Staff") {
+            header("location: staff_dashboard.php");
+        }
+    }
+}
+?>
