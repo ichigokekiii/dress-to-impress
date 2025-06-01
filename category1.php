2660c9972b31a1b1d9628511b25409ed9ba81d19
Cake <90282274+ichigokekiii@users.noreply.github.com>
2025-06-01 15:10:30 +0800

    Fixing Navbar
    AAAAAAAAAAAAAAAA

--- category1.php
+++ category1.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Category Selection - Dress to Impress</title>
+    <link rel="stylesheet" href="navbar.css">
     <link rel="stylesheet" href="category1.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
--- confirmVote.php
+++ confirmVote.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vote Confirmation - Dress to Impress</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="navbar.css">
     <link rel="stylesheet" href="confirmVote.css">
 </head>
 <body>
